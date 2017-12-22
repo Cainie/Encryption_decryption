03fce8cee7e8b857d9aba63578f5cef18b074041
Cainie <karol.marszalik@gmail.com>
2017-12-22 23:43:48 +0100

    Initial commit

---
+++ src/Sample.java
@@ -0,0 +1,40 @@
+import javax.crypto.Cipher;
+import java.security.*;
+
+public class Sample {
+    public static void main(String [] args) throws Exception {
+        // generate public and private keys
+        KeyPair keyPair = buildKeyPair();
+        PublicKey pubKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        // encrypt the message
+        byte [] encrypted = encrypt(privateKey, "This is a secret message");
+        System.out.println(new String(encrypted));  // <<encrypted message>>
+
+        // decrypt the message
+        byte[] secret = decrypt(pubKey, encrypted);
+        System.out.println(new String(secret));     // This is a secret message
+    }
+
+    public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+        final int keySize = 1024;
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(keySize);
+        return keyPairGenerator.genKeyPair();
+    }
+
+    public static byte[] encrypt(PrivateKey privateKey, String message) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+
+        return cipher.doFinal(message.getBytes());
+    }
+
+    public static byte[] decrypt(PublicKey publicKey, byte [] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+
+        return cipher.doFinal(encrypted);
+    }
+}
